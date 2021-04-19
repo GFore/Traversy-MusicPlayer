1282fd892fe679a0fac19ab705b0644ee2a14758
Greg Foreman <gfore@mindspring.com>
2021-04-18 23:59:22 -0400

    load songs

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ const progressContainer = document.querySelector('.progress-container');
 const audio = document.querySelector('#audio');
 const cover = document.querySelector('#cover');
 const title = document.querySelector('#title');
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+// Keep track of song
+let songIndex = 2;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+const loadSong = (song) => {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+};
