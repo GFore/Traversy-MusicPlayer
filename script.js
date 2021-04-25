28c2d060c5160827f6d9e20a8cc888caf95696c4
Greg Foreman <gfore@mindspring.com>
2021-04-25 11:48:32 -0400

    update progress indicator

--- script.js
+++ script.js
@@ -62,6 +62,12 @@ function nextSong() {
   playSong();
 };
 
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
   isPlaying ? pauseSong() : playSong();
@@ -70,3 +76,5 @@ playBtn.addEventListener('click', () => {
 // Change song events
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress);
