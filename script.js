e75d3ae610e80d341e4df34c1efcf1bbf83b1edf
Greg Foreman <gfore@mindspring.com>
2021-04-25 10:55:36 -0400

    add prev and next functionality

--- script.js
+++ script.js
@@ -36,11 +36,37 @@ function pauseSong() {
   musicContainer.classList.remove('play');
   playBtn.querySelector('i.fas').classList.add('fa-play');
   playBtn.querySelector('i.fas').classList.remove('fa-pause');
-  
+
   audio.pause();
+};
+
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
 };
 
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
   isPlaying ? pauseSong() : playSong();
 });
+
+// Change song events
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
