5f94a49e1d5be4eb88763e4398f9b7e068f68c1c
Greg Foreman <gfore@mindspring.com>
2021-04-27 23:43:19 -0400

    clean up

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
+const audio = document.querySelector('#audio');
+const cover = document.querySelector('#cover');
 const musicContainer = document.querySelector('.music-container');
 const nextBtn = document.querySelector('#next');
 const playBtn = document.querySelector('#play');
 const prevBtn = document.querySelector('#prev');
 const progress = document.querySelector('.progress');
 const progressContainer = document.querySelector('.progress-container');
-const audio = document.querySelector('#audio');
-const cover = document.querySelector('#cover');
 const title = document.querySelector('#title');
 
 // Song titles
 const songs = ['hey', 'summer', 'ukulele'];
 
 // Keep track of song
-let songIndex = 2;
+let songIndex = 0;
 
 // Initially load song details into DOM
 loadSong(songs[songIndex]);
@@ -84,9 +84,8 @@ playBtn.addEventListener('click', () => {
 // Change song events
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
-
 audio.addEventListener('timeupdate', updateProgress);
-
 progressContainer.addEventListener('click', setProgress);
 
+// Go to next song when the current song ends
 audio.addEventListener('ended', nextSong);
